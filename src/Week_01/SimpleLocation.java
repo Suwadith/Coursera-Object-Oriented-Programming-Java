99d75795231ff0e90b878ac7516fa99f33d59971
Suwadith <srithar.2015214@iit.ac.lk>
2017-05-01 12:08:06 +0530

    Constructor, Method Overloading!

--- src/Week_01/SimpleLocation.java
+++ src/Week_01/SimpleLocation.java
@@ -8,13 +8,27 @@ public class SimpleLocation {
     public double latitude;
     public double longitude;
 
+    //Default Constructor
+    public SimpleLocation(){
+        this.latitude = 32.9;
+        this.longitude = -117.2;
+    }
+
+    //Overloaded Constructor
     public SimpleLocation(double lat, double lon){
         this.latitude = lat;
         this.longitude = lon;
     }
 
+    //Normal Method which needs an object to calculate the distance
     public double distance(SimpleLocation other){
         //return getDist(this.latitude, this.longitude, other.latitude, other,longitude);
+        return 0.0;
+    }
+
+    //Overloaded Method which simple needs the end point Latitude and Longitude,
+    //so that it can use Default constructor's Latitude and Longitude as starting point Coordinates
+    public double distance(double otherLat, double otherLon){
         return 0.0;
     }
 
