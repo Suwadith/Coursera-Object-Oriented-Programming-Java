1c3f1ab9d1f4b10d21087477566f05b3a19a4875
Suwadith <srithar.2015214@iit.ac.lk>
2017-05-01 11:38:08 +0530

    Initial commit

---
+++ src/Week_01/SimpleLocation.java
@@ -0,0 +1,20 @@
+package Week_01;
+
+/**
+ * Created by suwad on 5/1/2017.
+ */
+public class SimpleLocation {
+
+    public double latitude;
+    public double longitude;
+
+    public SimpleLocation(double lat, double lon){
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+
+    public double distance(SimpleLocation other){
+        return getDist(this.latitude, this.longitude, other.latitude, other,longitude);
+    }
+
+}
